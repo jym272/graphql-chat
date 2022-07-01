cb13bd323dc0e5f959d70dce214502ebaa8830d6
jorge <jym272@gmail.com>
2022-07-01 16:09:48 -0300

    feat: websocket implementation in Apollo Client

--- client/src/graphql/client.js
+++ client/src/graphql/client.js
@@ -2,8 +2,11 @@ import {
   ApolloClient, ApolloLink, HttpLink, InMemoryCache
 } from 'apollo-boost';
 import { getAccessToken } from '../auth';
+import {getMainDefinition} from 'apollo-utilities';
+import {WebSocketLink} from "apollo-link-ws";
 
 const httpUrl = 'http://localhost:9000/graphql';
+const wsUrl = 'ws://localhost:9000/subscriptions';
 
 const httpLink = ApolloLink.from([
   new ApolloLink((operation, forward) => {
@@ -16,9 +19,20 @@ const httpLink = ApolloLink.from([
   new HttpLink({uri: httpUrl})
 ]);
 
+const wsLink = new WebSocketLink({uri: wsUrl, options: {reconnect: true, lazy: true}});
+
+const isSubscription = ({query}) => {
+    const definition = getMainDefinition(query);
+    return definition.kind === 'OperationDefinition' && definition.operation === 'subscription';
+}
+
 const client = new ApolloClient({
   cache: new InMemoryCache(),
-  link: httpLink,
+  link: ApolloLink.split(
+    isSubscription,
+    wsLink,
+    httpLink
+    ),
   defaultOptions: {query: {fetchPolicy: 'no-cache'}}
 });
 
